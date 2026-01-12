049931a466a284bcc3429f6f559ff078723067ea
이세한 <sehan8227@gmail.com>
2026-01-12 14:17:30 +0900

    <이세한>나야</이세한>을 출력하고싶어 변수에 따로 객체를 지정하려 출력을 원했으나 객체안에 문자열을 잘못입력하여 내가 출력하고자하는 출력이 아니게 되어 답을 보고 따라하려함.

--- stuct.py
+++ stuct.py
@@ -1,9 +1,19 @@
-body = [
-    {
-        "tag_name": "h1"
-        "tag_contents": "이세한"
-        "tag_style": {
-            "color":"blue"
-        }
-    }
-]
+practics = {
+    "left_angle_bracket": "<",
+    "right_angle_bracket": ">",
+    "tag_name": "",
+    "close_tag_slash": "/",
+    "children_elements": []
+}
+# <이세한>나야</이세한>을 만들어봐라
+#<>를 조합한 이름을 만들어야야하고 그사이에 나야라는 문자열을 넣고
+#</>를 조합한 이름을 넣는다.
+
+name = []
+name_1 = {"left_angle_bracket"},{"이세한"},{"right_angle_bracket"}
+its_me_mario = "나야"
+name_2 = {"left_angle_bracket"},{"이세한"},{"close_tag_slash"},{"right_angle_bracket"}
+
+print((name_1),(its_me_mario),(name_2))
+
+# print("<" + "공욱재" + ">" + "나야" + "<" + "/ + "공욱재" + ">") 이답은 이렇게된다?
